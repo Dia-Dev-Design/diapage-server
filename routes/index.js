b1271bf611811b572a7c16e4f91ed9ff9dbb07b5
Dustin Hurtt <dustin_hurtt@outlook.com>
2024-10-02 11:37:29 -0400

    completed information request functionality, needs styling

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,64 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const transporter = require('../configs/nodemailer')
+const transporter = require("../configs/nodemailer");
 
-router.post('/send-email', async (req, res) => {
-
+router.post("/send-email", async (req, res) => {
   try {
-    // const { recipientEmail, subject } = req.body;
+    const { emailContent, subjectsSelected } = req.body;
 
-    subject = "Testing Route!!!"
+    const subject = `We've received a message from ${emailContent.name}`;
+
+    let interests = "";
+
+    subjectsSelected.forEach((subject, i) => {
+      if (subjectsSelected.length === 1) {
+        interests += ` ${subject}`
+      } else {
+        if (i < subjectsSelected.length - 1) {
+          interests += ` ${subject},`;
+        } else {
+          interests += ` and ${subject}`;
+        }
+
+      }
+    });
 
     const html = `<div>
                     <h1>We've received an information request!</h1>
-                    <p>Thank you for contacting Día Dev & Design! We're excited to have you on board!</p>
-                    <p>This is a Test</p>
-                    <p>The Día Team</p>
-                  </div>`
-  
+                    <p>${emailContent.name ? emailContent.name : "No name provided"} is interested in${interests ? interests : "nothing specific"}.</p>
+                    ${
+                      emailContent.message
+                        ? `<p>They've left us the following messeage:</p>
+                       <p>${emailContent.message}
+                      `
+                        : `<p>They did not provide a message</p>`
+                    }
+                    ${
+                      emailContent.email
+                        ? `<p>Their return email address is ${emailContent.email}</p>`
+                        : `They did not provide an email address.`
+                    }
+                  </div>`;
+
     const mailOptions = {
       from: "info@diadevdesign.com",
       to: "info@diadevdesign.com",
       subject,
       html,
     };
-  
-      await transporter.sendMail(mailOptions);
-      res.status(200).json({ message: 'Email sent successfully!' });
-    } catch (error) {
-      console.error('Error sending email:', error);
-      res.status(500).json({ error: 'Failed to send email' });
-    }
-  });
-  
+
+    await transporter.sendMail(mailOptions);
+    res
+      .status(200)
+      .json({
+        message:
+          "Message sent successfully! \nWe will be in touch with you soon",
+      });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ error: "Failed to send email" });
+  }
+});
+
 module.exports = router;
