d7205d4e422395aa9dccea5d23b2a2f15bbca9dc
Dustin Hurtt <dustin_hurtt@outlook.com>
2025-01-12 13:41:20 -0500

    fixed nodemailer issue again, ran production package intstalls, dustin 1411122025

--- configs/nodemailer.js
+++ configs/nodemailer.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT * 1,
-    secure: false,// true for 465, false for other ports
+    // secureConnection: false,// true for 465, false for other ports
     auth: {
         user: process.env.SMTP_AUTH_USER, // your GoDaddy email
         pass: process.env.SMTP_AUTH_PASS // your GoDaddy email password
